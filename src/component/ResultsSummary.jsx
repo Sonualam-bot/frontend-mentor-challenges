864addf1db783cd9daa4ff85e032dd29b1ab03e7
Sonualam <sonualam1599@gmail.com>
2023-11-21 10:33:43 +0530

    style improved responsiveness

--- src/component/ResultsSummary.jsx
+++ src/component/ResultsSummary.jsx
@@ -6,39 +6,39 @@ import visualIcon from "../assets/images/icon-visual.svg";
 function ResultsSummary() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <div className="flex flex-col sm:flex-row   md:w-[650px]   md:h-[450px] sm:h-full justify-center items-center md:pr-7 rounded-3xl text-[18px] md:gap-9  md:shadow-lg font-sans  sm:shadow-none ">
+      <div className="flex flex-col sm:flex-row w-full md:w-[650px] md:h-[450px] sm:h-full justify-start items-start  rounded-3xl text-[18px] md:shadow-lg font-sans sm:shadow-none">
         {/* left part starts here */}
-        <div className=" flex flex-col  justify-around w-full sm:w-full   h-[450px]  rounded-2xl p-6 items-center bg-gradient-to-b from-[--light-slate-blue] to-[--light-royal-blue] text-white">
+        <div className="flex flex-col justify-around w-full sm:w-full h-[450px] md:rounded-2xl sm:rounded-t-none p-6 items-center bg-gradient-to-b from-[--light-slate-blue] to-[--light-royal-blue] text-white">
           <div>
-            <h2 className="text-2xl font-semibold text-[--light-lavender]  ">
+            <h2 className="text-2xl font-semibold text-[--light-lavender]">
               Your Result
             </h2>
           </div>
-          <div className="h-[140px] w-[140px] rounded-[50%] shadow-inner  flex flex-col items-center justify-center  bg-gradient-to-b from-[--violet-blue-circle] to-[ --persian-blue-circle] ">
+          <div className="h-[140px] w-[140px] rounded-[50%] shadow-inner flex flex-col items-center justify-center bg-gradient-to-b from-[--violet-blue-circle] to-[--persian-blue-circle]">
             <p className="text-[--white] text-6xl font-bold ">76</p>
             <p className="text-sm text-[--light-lavender] font-medium">
               of 100
             </p>
           </div>
           <div className="text-center">
-            <h1 className="text-4xl text-[--white] font-semibold mb-4 ">
+            <h1 className="text-4xl text-[--white] font-semibold mb-4">
               Great
             </h1>
-            <p className=" text-[--light-lavender]">
+            <p className="text-[--light-lavender]">
               You Scored higher than 65% of
             </p>
-            <p className=" text-[--light-lavender]">
+            <p className="text-[--light-lavender]">
               {" "}
               the people who have taken{" "}
             </p>
-            <p className=" text-[--light-lavender]">these tests.</p>
+            <p className="text-[--light-lavender]">these tests.</p>
           </div>
         </div>
         {/* left part ends here */}
-        {/* right part starts  here */}
-        <div className=" w-full sm:w-full h-[450px] flex flex-col gap-7 py-11 ">
+        {/* right part starts here */}
+        <div className="w-full  h-[450px] flex flex-col gap-7 py-11 px-5  ">
           <div>
-            <h1 className="font-bold text-xl text-[--dark-gray-blue] ">
+            <h1 className="font-bold text-xl text-[--dark-gray-blue]">
               Summary
             </h1>
           </div>
@@ -97,7 +97,7 @@ function ResultsSummary() {
             </div>
           </div>
           <div>
-            <button className="p-2 bg-[--dark-gray-blue] w-full rounded-3xl text-white cursor-pointer ">
+            <button className="p-2 bg-[--dark-gray-blue] w-full rounded-3xl text-white cursor-pointer">
               Continue
             </button>
           </div>
